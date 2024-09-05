129bf0b17930cd60a5c20cb6c3de12f12618732f
Uanderson Ricardo <uandersonrfs@gmail.com>
2024-09-04 22:40:01 -0300

    build: add ts with express server

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "extends": [
+    "plugin:prettier/recommended",
+    "prettier",
+    "eslint:recommended"
+  ],
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2022,
+    "sourceType": "module",
+    "project": "tsconfig.json",
+    "tsconfigRootDir": __dirname
+  },
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "ignorePatterns": [".eslintrc.cjs"],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+}
