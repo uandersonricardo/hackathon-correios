7a39ea07b99f1685fc37ed6a38feeecc42ad6ca5
Uanderson Ricardo <uandersonrfs@gmail.com>
2024-09-06 14:07:59 -0300

    feat: homescreen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,46 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ArrowRightIcon } from 'lucide-vue-next'
 </script>
 
 <template>
-  <header class="w-full bg-neutral-300 p-2 flex items-center gap-2 justify-between">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
-    <nav class="flex gap-2">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+  <header class="w-full bg-secondary-50">
+    <div class="container mx-auto p-3 flex items-center gap-2 justify-between">
+      <img alt="Correios logo" class="h-5" src="@/assets/correios.svg" />
+      <nav class="flex gap-8 text-primary-30">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
   </header>
+
+  <section class="w-full bg-secondary-50">
+    <div class="container mx-auto flex flex-col items-center justify-center text-center my-20">
+      <p class="font-medium text-secondary-20 uppercase mb-2">
+        Nossa plataforma de apoio ao atendente
+      </p>
+      <h1 class="text-6xl font-bold text-primary-20 mb-8">Como podemos te ajudar?</h1>
+      <div class="w-full max-w-xl flex items-center px-1 bg-white rounded-full">
+        <input
+          type="text"
+          placeholder="Digite qualquer dúvida"
+          class="flex-grow py-3 px-6 border-none focus:outline-none text-gray-800 bg-transparent"
+        />
+        <button
+          class="bg-primary-30 hover:bg-primary-20 text-white font-semibold rounded-full h-10 w-10 flex justify-center items-center"
+        >
+          <ArrowRightIcon :size="20" />
+        </button>
+      </div>
+      <div class="text-sm mt-6 text-secondary-20 gap-2 flex items-center">
+        <span>Buscas populares:</span>
+        <span class="px-3 py-1 bg-secondary-30/50 rounded-full text-white">atraso</span>
+        <span class="px-3 py-1 bg-secondary-30/50 rounded-full text-white">rota</span>
+        <span class="px-3 py-1 bg-secondary-30/50 rounded-full text-white">remessa</span>
+        <span class="px-3 py-1 bg-secondary-30/50 rounded-full text-white">extravio</span>
+      </div>
+    </div>
+  </section>
 
   <RouterView />
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,6 @@ const animate = require('tailwindcss-animate')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
-  safelist: ['dark'],
   prefix: '',
 
   content: [
@@ -22,19 +20,95 @@ module.exports = {
       }
     },
     extend: {
+      fontFamily: {
+        sans: ['Trebuchet MS', 'arial', 'helvetica', 'Open Sans', 'sans-serif']
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
+        correios: {
+          DEFAULT: '#ffe600',
+          dark: '#00416b',
+          light: '#007bb3'
         },
+        primary: {
+          0: '#000000',
+          10: '#001F33',
+          20: '#00416B',
+          30: '#006399',
+          40: '#0083CA',
+          50: '#00A5FF',
+          60: '#33B7FF',
+          70: '#66C9FF',
+          80: '#99DBFF',
+          90: '#CCEDFF',
+          95: '#E5F6FF',
+          99: '#FAFDFF',
+          100: '#FFFFFF'
+        },
+
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
+          0: '#000000',
+          10: '#59410D',
+          20: '#8E640B',
+          30: '#CD992B',
+          40: '#FFC20E',
+          50: '#FFD400',
+          60: '#FFE600',
+          70: '#FFF34D',
+          80: '#FFF780',
+          90: '#FFF7B3',
+          95: '#FFFDE5',
+          99: '#FFFFFA',
+          100: '#FFFFFF'
+        },
+        error: {
+          0: '#000000',
+          10: '#3D0000',
+          20: '#700000',
+          30: '#A30000',
+          40: '#D50000',
+          50: '#FF0A0A',
+          60: '#FF3D3D',
+          70: '#FF7070',
+          80: '#FFA3A3',
+          90: '#FFD6D6',
+          95: '#FFEBEB',
+          99: '#FFFAFA',
+          100: '#FFFFFF'
+        },
+        neutral: {
+          0: '#000000',
+          10: '#1E1D1B',
+          20: '#3F3E3B',
+          30: '#5E5C5A',
+          40: '#72706E',
+          50: '#9B9996',
+          60: '#BAB8B5',
+          70: '#DEDCD9',
+          80: '#EDEBE8',
+          90: '#EDEBE8',
+          95: '#FBF9F6',
+          99: '#FDFDFB',
+          100: '#FFFFFF'
+        },
+        'neutral-variant': {
+          0: '#000000',
+          10: '#3C3630',
+          20: '#4C433C',
+          30: '#584F46',
+          40: '#675D51',
+          50: '#807364',
+          60: '#998E82',
+          70: '#BDB4AB',
+          80: '#D1CCC7',
+          90: '#EFEDEA',
+          95: '#F9F8F6',
+          99: '#FDFDFC',
+          100: '#FFFFFF'
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
